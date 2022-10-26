c0f739bfa1e9ac03265af34808c11e5032bfb9d4
liu <ldbzqyzz@gmail.com>
2022-10-26 19:58:59 +0800

    测试修改

--- Apps/goods/views.py
+++ Apps/goods/views.py
@@ -3,9 +3,12 @@
 # CreateTime: 2022/10/14 22:41
 
 from . import good_print
+from Models.UserModel import User, db
 
 
 @good_print.route("/")
 def hello():
+    r = db.session.query(User.id).filter(User.name == "张三").all()
+    print(222, r, 111)
     return "goods hello"
 
--- config.py
+++ config.py
@@ -4,4 +4,4 @@
 
 
 class DataBaseConfig:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@127.0.0.1:3306/newproj"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@127.0.0.1:3306/newproj"
